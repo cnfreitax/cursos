cad958b7ae9fbbe6eb005e80b88d2bd3f4acaba8
cnfreitax <60153670+cnfreitax@users.noreply.github.com>
2020-02-26 16:57:16 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,62 @@
+import React, { Component, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import Tabela from './Tabela';
+import Form from './Formulario';
+import Header from './Header';
+
+class App extends Component {
+  state = {
+    autores: [
+       {
+         nome: 'Paulo',
+         livro: 'React',
+         preco: '1000'
+       },
+       {
+         nome: 'Daniel',
+         livro: 'Java',
+         preco: '99'
+       },
+       {
+         nome: 'Marcos',
+         livro: 'Design',
+         preco: '150'
+       },
+       {
+         nome: 'Bruno',
+         livro: 'DevOps',
+         preco: '100'
+       }
+     ],
+  };
+
+  removeAutor = index =>{
+
+    const { autores } = this.state;
+
+    this.setState(
+      {
+        autores: autores.filter((autor, posAutal) =>{
+            return posAutal !== index;
+        }),
+      }
+    )
+  };
+
+  escutadorDeSubmit = autor => {
+    this.setState({autores:[...this.state.autores, autor]})
+  }
+ render(){
+  return (
+    <Fragment>
+      <Header />
+      <div className="container">
+          <Tabela autores = { this.state.autores } removeAutor=  { this.removeAutor}/>
+          <Form  escutadorDeSubmit = { this.escutadorDeSubmit } />
+      </div>
+    </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <nav>
+            <div className="nav-wrapper indigo ligthen-2">
+              <a href="https://google.com.br" className="brand-logo">Casa do Saber</a>
+              <ul className="right">
+                <li><a href="https://google.com.br">Sobre</a></li>
+                <li><a href="https://google.com.br">Produtos</a></li>
+                <li><a href="https://google.com.br">Contato</a></li>
+              </ul>
+            </div>
+          </nav>
+      );
+}
+
+export default Header;
